a48f431143f97c4c5171d31454f10711a9de35bb
abar-1 <aneeshbargaje@gmail.com>
2025-06-01 21:23:05 -0500

    Header

---
+++ src/app/Components/PersonalCard.js
@@ -0,0 +1,16 @@
+"use client";
+
+import React from 'react';
+import './personalCard.css';
+
+export default function personalCard() {
+
+    return(
+        <div className="container">
+            <h2>Hi, my name is Aneesh <span className="wave"> 👋🏽</span></h2>
+
+        </div>
+
+    );
+
+}
