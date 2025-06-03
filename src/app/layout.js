d6ff91f04446d26dc0f2166da47ec50d85cbf7e4
abar-1 <aneeshbargaje@gmail.com>
2025-06-03 18:23:52 -0500

    Personal Card

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "./Components/Header";
+import PersonalCard from "./Components/PersonalCard";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,8 +21,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+      <body>
+        <div>
+        <Header />
+        </div>
+        <div>
+        <PersonalCard />
+        </div>
+        
       </body>
     </html>
   );
