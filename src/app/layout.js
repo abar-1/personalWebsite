b3c0e426a2dbc273e7bf1646128fbd8210d462eb
abar-1 <aneeshbargaje@gmail.com>
2025-06-07 16:14:08 -0500

    Formatting

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 export const metadata = {
   title: 'Aneesh Bargaje | Portfolio',
   description: 'Software Developer, Researcher, Cybersecurity Intern',
+
+  viewport: 'width=device-width, initial-scale=1.0',
 };
 
 export default function RootLayout({ children }) {
