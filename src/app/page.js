d60131eac302e5602b38fbe1f44b5a9ab2080b57
Aneesh Bargaje <aneeshbargaje@gmail.com>
2025-11-27 20:20:14 -0600

    SEO

--- src/app/page.js
+++ src/app/page.js
@@ -14,6 +14,12 @@ export default function HomePage() {
         <head>
           <link rel="icon" type="image/x-icon" href="https://media.licdn.com/dms/image/v2/D5603AQGMO-97LyMqCw/profile-displayphoto-shrink_400_400/B56ZbuUBUaHUAg-/0/1747754948812?e=1754524800&v=beta&t=7pfDPvrMjidyrBCUzDN_SASE6Yn-3VGGjphtR8qEUOI" />
           <title>Aneesh&apos;s Portfolio</title>
+          <meta name="description" content="Aneesh's personal portfolio showcasing projects, publications, and experience." />
+          <meta name="keywords" content="Aneesh, Bargaje, Portfolio, Projects, Publications, Experience, Developer, Aneesh Bargaje" />
+          <meta name="author" content="Aneesh Bargaje" />
+          <meta property="og:title" content="Aneesh Bargaje | Portfolio" />
+          <meta property="og:description" content="ML, cybersecurity, software projects, and aviation research." />
+          <meta property="og:type" content="website"></meta>
         </head>
         <body>
          
